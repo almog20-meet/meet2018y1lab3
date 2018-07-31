09e1d480a7f50c15d0ca26ea6fca977dcf65ccd4
almog20-meet <almog20@meet.mit.edu>
2018-07-31 03:26:45 -0400

    Stopping here

---
+++ HelloWorld.py
@@ -0,0 +1,15 @@
+language= "Italian"
+translation= "Ciao Mondo"
+print (("The translation of hello world in ") + (language) + (" is ") + (translation))
+
+language1= "Esperanto"
+translation1= "Saluton Mondo"
+print (("The translation of hello world in ") + (language1) + (" is ") + (translation1))
+
+language2= "Xhosa"
+translation2= "Molo Lizwe"
+print (("The translation of hello world in ") + (language2) + (" is ") + (translation2))
+
+language3= "Zolo"
+translation3= "Sawubona Mhlaba"
+print (("The translation of hello world in ") + (language3) + (" is ") + (translation3))
